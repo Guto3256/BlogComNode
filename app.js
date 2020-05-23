8b1bafd99b206f8a5ce004470cbdb48edf6ae463
guto3256 <guilherme.abreu378@gmail.com>
2020-05-23 15:48:56 -0300

    Listando, editando e excluindo categorias

--- app.js
+++ app.js
@@ -6,8 +6,22 @@
 	const admin = require('./routes/admin');
 	const path = require('path');
 	const mongoose = require('mongoose');
-
+	const session = require('express-session');
+	const flash = require('connect-flash');
 //Configurações
+	// Sessão
+		app.use(session({
+			secret: "nodeBlog2020",
+			resave: true,
+			saveUninitialized: true
+		}));
+		app.use(flash());
+	// Midleware
+		app.use((req, res, next) => {
+			res.locals.success_msg = req.flash("success_msg");
+			res.locals.error_msg = req.flash("error_msg");
+			next();
+		});
 	// Body parser
 		app.use(bodyParser.urlencoded({extended: true}));
 		app.use(bodyParser.json());
--- routes/admin.js
+++ routes/admin.js
@@ -7,31 +7,113 @@
 
 
 // Criando as rotas
+  //Rota principal
   router.get('/', (req, res) => {
     res.render("admin/index");
   });
+
+  //Rota de Posts
   router.get('/posts', (req, res) => {
     res.send("Página dos posts");
   });
-  router.get('/categorias', (req, res) => {
-    res.render('admin/categorias');
-  });
-  router.get('/categorias/add', (req, res) => {
-    res.render('admin/addCategoria');
-  });
-  router.post('/categorias/nova', (req, res) => {
-    const novaCategoria = {
-      Nome: req.body.nome,
-      Slug: req.body.slug
-    }
 
-    new Categoria(novaCategoria).save().then(() => {
-      console.log("Categoria salva com sucesso!");
+  //Rota de categorias
+  router.get('/categorias', (req, res) => {
+    Categoria.find().lean().sort({Data: "desc"}).then((Categorias) => {
+      res.render('admin/categorias', {Categorias: Categorias});
     }).catch((erro) => {
-      console.log("Erro ao salvar categoria. Erro: " + erro);
+      req.flash("error_msg", "Erro ao carregar categorias!");
+      res.redirect("/admin");
     });
   });
 
+  //Rota do formulário de cadastro de categorias
+  router.get('/categorias/add', (req, res) => {
+    res.render('admin/addCategoria');
+  });
 
+  //Rota de cadastro de categorias
+  router.post('/categorias/nova', (req, res) => {
+    var erros = [];
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+      erros.push({text: "Nome inválido"});
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+      erros.push({text: "Slug inválido"});
+    }
+
+    if(erros.length > 0){
+      res.render("admin/addCategoria", {erros: erros});
+    }else{
+      const novaCategoria = {
+        Nome: req.body.nome,
+        Slug: req.body.slug
+      }
+
+      new Categoria(novaCategoria).save().then(() => {
+        req.flash("success_msg", "Categoria criada com sucesso!");
+        res.redirect("/admin/categorias");
+      }).catch((erro) => {
+        req.flash("error_msg", "Erro ao criar categoria, tente novamente!");
+        res.redirect("/admin");
+      });
+    }
+  });
+
+  //Rota do formulário de edição de categorias
+  router.get('/categorias/edit/:id', (req, res) => {
+    Categoria.findOne({_id:req.params.id}).lean().then((Categoria) => {
+      res.render('admin/editCategoria', {Categoria: Categoria});
+    }).catch((erro) => {
+      req.flash("error_msg", "Esta categoria não existe!");
+      res.redirect("/admin/categorias");
+    });
+  });
+
+  //Rota de edição de categorias
+  router.post('/categorias/edit', (req, res) => {
+    var erros = [];
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+      erros.push({text: "Nome inválido"});
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+      erros.push({text: "Slug inválido"});
+    }
+
+    if(erros.length > 0){
+      res.render("admin/editCategoria", {erros: erros});
+    }else{
+      Categoria.findOne({_id: req.body.id}).then((categoria) => {
+        categoria.Nome = req.body.nome;
+        categoria.Slug = req.body.slug;
+
+        categoria.save().then(() => {
+          req.flash("success_msg", "Categoria editada com sucesso!");
+          res.redirect("/admin/categorias");
+        }).catch((erro) => {
+          req.flash("error_msg", "Erro ao salvar edição da categoria!");
+          res.redirect("/admin/categorias");          
+        });
+      }).catch((erro) => {
+        req.flash("error_msg", "Erro ao editar categoria!");
+        res.redirect("/admin/categorias");
+      });
+    }
+  });
+
+  //Rota de exclusão de categorias
+  router.get('/categorias/del/:id', (req, res) => {
+    Categoria.remove({_id: req.params.id}).then(() => {
+      req.flash("success_msg", "Categoria deletada com sucesso!");
+      res.redirect("/admin/categorias");
+    }).catch((erro) => {
+      req.flash("success_msg", "Ocorreu um erro ao deletar a categoria, tente novamente!");
+      res.redirect("/admin/categorias");
+    });
+  });
 // Exporta as rotas
   module.exports = router;
